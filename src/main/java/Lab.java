4a782cb68dbe6ae391c1b6bd051ab62c4f2e866d
osadeyemo <osadeyemo@yahoo.com>
2023-02-27 20:40:40 +0000

    ForLoop Lab Completed

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -42,7 +42,10 @@ public class Lab {
     public String returnNumbers(int start, int end){
         String buildingString = "";
 
-//        code here
+        // code here
+        for (int i = start; i < end; i++) {
+            buildingString = buildingString + i + " ";
+        }
 
         return buildingString;
     }
